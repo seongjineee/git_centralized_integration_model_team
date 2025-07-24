309f3250e5cb7bf6bf17b2f8a4e853d6412750c4
“yooseungmin9” <2win0min@naver.com>
2025-07-24 16:00:04 +0900

    adv버전 추가

---
+++ Calculator_ADV.java
@@ -0,0 +1,49 @@
+public class Calculator {
+
+import java.util.Scanner;
+
+    public static double performOperation(double num1, double num2, char operator) {
+        double result = 0.0;
+        
+        //알고리즘 추가
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+       
+        }
+
+        return result;
+    }
+    public static void getInputAndCalculate() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("간단한 콘솔 계산기 프로그램");
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.print("사용할 연산자를 입력하세요 (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        double result = Calculator.performOperation(num1, num2, operator);
+
+        if (!Double.isNaN(result)) {
+            System.out.println("결과: " + result);
+        }
+
+        scanner.close();
+        }
+    }

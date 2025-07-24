400b83a1f43e719c97c45b40713cc8ec4fafa8fa
seongjineee <sungjin8506@naver.com>
2025-07-24 15:47:45 +0900

    개발완료

---
+++ Calculator.java
@@ -0,0 +1,28 @@
+public class Calculator {
+    public static double performOperation(double num1, double num2, char operator) {
+        double result = 0.0;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("0으로 나눌 수 없습니다.");
+                }
+                break;
+            default:
+                System.out.println("올바른 연산자를 입력하세요.");
+        }
+
+        return result;
+    }
+}
